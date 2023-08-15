1efb4c4b7183e5c8fb91475bbc93f53398bf9ab1
POPcon441 <139322582+POPcon441@users.noreply.github.com>
2023-08-15 15:41:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -132,6 +132,22 @@ def convert_hybrid_words(text):
     text = text.replace(' Station-ro', 'Station-ro')
     
     return text
+    
+def levenshtein_distance(s1, s2):
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    distances = list(range(len(s1) + 1))
+    for index2, char2 in enumerate(s2):
+        new_distances = [index2 + 1]
+        for index1, char1 in enumerate(s1):
+            if char1 == char2:
+                new_distances.append(distances[index1])
+            else:
+                new_distances.append(1 + min(distances[index1], distances[index1 + 1], new_distances[-1]))
+        distances = new_distances
+
+    return distances[-1]
 
 def perform_address_search(search_data):
     api_key = 'devU01TX0FVVEgyMDIzMDcyODE1MzkzNzExMzk3MzA='
